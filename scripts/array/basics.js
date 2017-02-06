6cd1162bd433a488b9880b0456a95caf897842be
bhawnapathak <bhawna9123@gmail.com>
2017-02-06 14:49:33 +0530

    Added function to print an array in ascending order.

--- scripts/array/basics.js
+++ scripts/array/basics.js
@@ -1,24 +1,46 @@
-var array = [2, 3, 5, 8, 9, 2];
+var array = [2, 3, 0, 1, 5, 8, 9, 2];
 console.log(array);
 
 // printArr(array);
-printArrReverse(array);
+// printArrReverse(array, '#');
+printArrAscending(array, '<');
 
 
 ///////////////////////////// Functions /////////////////////////////
-function printArrReverse(array) {
+function arrayAscending(array) {
+    for (var i = 0; i < array.length; i++) {
+        for (var j = i + 1; j < array.length; j++) {
+            if (array[j] < array[i]) {
+                var temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+function printArrAscending(array, separator) {
+    separator = separator || " ";
+    var arrayAsc = arrayAscending(array);
+    printArr(arrayAsc, separator);
+}
+
+function printArrReverse(array, separator) {
+    separator = separator || " ";
     var str = "";
     for (var i = array.length - 1; i >= 0; i--) {
-        str = str + array[i] + ' ';
+        str = str + array[i] + separator;
     }
     str = str.substring(0, str.length - 1);
     console.log(str);
 }
 
-function printArr(array) {
+function printArr(array, separator) {
+    separator = separator || " ";
     var str = "";
     for (var i = 0; i < array.length; i++) {
-        str = str + array[i] + ' ';
+        str = str + array[i] + separator;
     }
     str = str.substring(0, str.length - 1);
     console.log(str);
