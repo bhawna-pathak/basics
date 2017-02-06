9e9c018edf452a4bc8856247679b1bb59b01b1aa
bhawnapathak <bhawna9123@gmail.com>
2017-02-06 14:09:35 +0530

    Added function to print an array in reverse order.

--- scripts/array/basics.js
+++ scripts/array/basics.js
@@ -1,10 +1,19 @@
 var array = [2, 3, 5, 8, 9, 2];
 console.log(array);
 
-printArr(array);
+// printArr(array);
+printArrReverse(array);
 
 
 ///////////////////////////// Functions /////////////////////////////
+function printArrReverse(array) {
+    var str = "";
+    for (var i = array.length - 1; i >= 0; i--) {
+        str = str + array[i] + ' ';
+    }
+    str = str.substring(0, str.length - 1);
+    console.log(str);
+}
 
 function printArr(array) {
     var str = "";
