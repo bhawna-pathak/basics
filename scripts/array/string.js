a243918dc52df124eaaec67af622c1972c06d56a
bhawnapathak <bhawna9123@gmail.com>
2017-02-07 12:20:13 +0530

    function to check the number is even or odd.

--- scripts/array/string.js
+++ scripts/array/string.js
@@ -1,17 +1,24 @@
-// var str = "hello how are you";
-// console.log(str);
-
-removeSpace("my name is bhawna");
+// removeSpace("my name is bhawna");
+evenOddNumber(5);
 
 ///////////////////////////// Functions /////////////////////////////
 function removeSpace(string) {
     var str = '';
-    var count = 0;
     for (var i = 0; i < string.length; i++) {
         if (string[i] == " ") {
             string = string.replace(/ /g, '');
         }
         str = str + string[i];
     }
-    console.log(str);
+    return str;
 }
+
+function evenOddNumber(num) {
+    if (num % 2 == 0) {
+        console.log('Even Number');
+    } else {
+        console.log('Odd Number')
+    }
+}
+
+
