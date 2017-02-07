87aea78279d883f9100d486f835f006b9adb7f92
bhawnapathak <bhawna9123@gmail.com>
2017-02-07 19:28:17 +0530

    Function to print fibonacci series and count charaters of a string

--- scripts/array/string.js
+++ scripts/array/string.js
@@ -1,24 +1,58 @@
-// removeSpace("my name is bhawna");
-evenOddNumber(5);
+var str = 'my name is bhawna';
+// removeSpace(str);
+// getStringCount(str);
+evenOddNumber();
+// fibonacci(15);
 
 ///////////////////////////// Functions /////////////////////////////
-function removeSpace(string) {
-    var str = '';
-    for (var i = 0; i < string.length; i++) {
-        if (string[i] == " ") {
-            string = string.replace(/ /g, '');
+function removeSpace(str) {
+    var newString = '';
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == " ") {
+            newString = str.replace(/ /g, '');
         }
-        str = str + string[i];
     }
-    return str;
+    return newString;
 }
+
+function getStringCount(str) {
+    var uniqueStr = [];
+    var charCount = [];
+    var obj = [];
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == " ") {
+            var str = removeSpace(str);
+        }
+        var currenPos = uniqueStr.indexOf(str[i]);
+        if (currenPos == -1) {
+            uniqueStr.push(str[i]);
+            charCount.push(1);
+        } else {
+            charCount[currenPos] = charCount[currenPos] + 1;
+        }
+    }
+    for (var i = 0; i < uniqueStr.length; i++) {
+        obj[uniqueStr[i]] = charCount[i];
+    }
+    return obj;
+}
+
 
 function evenOddNumber(num) {
+    var num = 2;
     if (num % 2 == 0) {
         console.log('Even Number');
-    } else {
-        console.log('Odd Number')
+        return;
     }
+    console.log('Odd Number');
 }
 
-
+function fibonacci(len) {
+    var array = [];
+    array[0] = 0;
+    array[1] = 1;
+    for (var i = 2; i < len; i++) {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    console.log(array);
+}
