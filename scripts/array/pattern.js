867d6ac0340aef528540baac06795edc49b3dd9b
bhawnapathak <bhawna9123@gmail.com>
2017-02-09 15:45:13 +0530

    Print Invert triangle

--- scripts/array/pattern.js
+++ scripts/array/pattern.js
@@ -1,10 +1,19 @@
-// printAstricTriangle();
 ///////////////////////////// Functions /////////////////////////////
-function printAstricTriangle(row) {
-    for (var i = 0; i < row; i++) {
+function printAstricTriangle(length) {
+    for (var i = 0; i < length; i++) {
         for (var j = 0; j <= i; j++) {
             document.write('*');
         }
         document.write("<br>");
+    }
+}
+
+function printInvertAstrictTriangle(length) {
+    for (var i = length; i >= 1; i--) {
+        for (var j = 1; j <= i; j++) {
+            document.write('*');
+        }
+        document.write("<br>");
+
     }
 }
